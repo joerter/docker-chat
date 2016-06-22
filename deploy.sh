7f280f0688d0876858356d3908e1cfd204f0e704
John Oerter <joerter@gmail.com>
2016-06-22 07:11:50 -0500

    Try out deploying

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-rsync docker-compose.production.yml chat@104.236.74.142:/home/chat
+#! /bin/bash
+
+rsync ./docker-compose.production.yml chat@chat.johnoerter.me:/home/chat
+ssh chat@chat.johnoerter.me docker-compose -f docker-compose.production.yml stop
+ssh chat@chat.johnoerter.me docker-compose -f docker-compose.production.yml build chat
+ssh chat@chat.johnoerter.me docker-compose -f docker-compose.production.yml up
